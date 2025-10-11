3c2ef176429422da743122e9ccb2c72533da15f6
Viswajith Muraleedharan <viswajithmuralee@gmail.com>
2025-10-11 17:55:48 +0530

    second lesson

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const parent = React.createElement("div", { id: "parent" }, 
     React.createElement("div", { id: "child" }, 
-        [React.createElement("h1", { id: "h1tag" }, "This is an h1 tag"), React.createElement("h2", { id: "h2tag" }, "This is an h2 tag")]))
+        [React.createElement("h1", { id: "h1tag" }, "I a Viswajith"), React.createElement("h2", { id: "h2tag" }, "We")]))
 
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl);
